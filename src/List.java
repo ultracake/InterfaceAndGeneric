12df4f56a723d26739661d9eab1c08d105137d41
ultracake <jackievuong@live.dk>
2019-01-04 10:28:36 +0100

    extra comment

--- src/List.java
+++ src/List.java
@@ -1,17 +1,18 @@
-public interface List<E> {
-
+//E is a Object!
+public interface List<E>
+{
     public int size();
 
     public Object get(int index);
 
     public String toString();
 
-    //public int indexOf(ListNode node);
-
     public void add(E value);
 
     public void add(int index, E value);
 
     public void remove(int index);
-
 }
+
+
+
